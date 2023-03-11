5b843f653f64b19d2f973a06179fa3da3e811ae5
Shaddy Mansour <shaddyhm@outlook.com>
2023-03-11 09:03:35 -0500

    add docs and pre-commit hook

--- src/result.ts
+++ src/result.ts
@@ -1,11 +1,23 @@
 export type PotentialPromise<T> = Promise<T> | T;
 
-export class Result<T> {
+/**
+ * Result railway,
+ * it is one way to make code look nice and tidy.
+ */
+export class Result<T extends object> {
   private error: Error;
   private isSuccess: boolean;
   private queue: Array<() => Promise<void>>;
   private value: T;
 
+  /**
+   * Validation step passing the encapsulated value.
+   * The function return true if the validation pass and false otherwise.
+   * When validation fails, the msg will be wrapped into an error object.
+   * @param {(v: T) => PotentialPromise<boolean>} fn 
+   * @param {string} msg 
+   * @returns {Result<T>}
+   */
   public ensure(
     fn: (v: T) => PotentialPromise<boolean>,
     msg: string
@@ -22,10 +34,20 @@ export class Result<T> {
     return this;
   }
 
-  public static from<T>(v: T): Result<T> {
+  /**
+   * Return a new instance of Result.
+   * @param {T extends object} v - an object
+   * @returns {Result<T>} 
+   */
+  public static from<T extends object>(v: T): Result<T> {
     return new Result(v);
   }
 
+  /**
+   * Get the encapsulated value or the default parameter value.
+   * @param {any} v - any value
+   * @returns {Promise<T | U>}
+   */
   public async getOrDefault<U>(v: U): Promise<T | U> {
     await this.queue.reduce((p, fn) => p.then(fn), Promise.resolve());
     if (!this.isSuccess) {
@@ -34,6 +56,10 @@ export class Result<T> {
     return this.value;
   }
 
+  /**
+   * Get the encapsulated value when there is no error or throw the existing error.
+   * @returns {Promise<T>}
+   */
   public async getOrThrow(): Promise<T> {
     await this.queue.reduce((p, fn) => p.then(fn), Promise.resolve());
     if (!this.isSuccess) {
@@ -42,7 +68,12 @@ export class Result<T> {
     return this.value;
   }
 
-  public map<U>(fn: (v: T) => PotentialPromise<U>): Result<U> {
+  /**
+   * Transform the encapsulated object to another.
+   * @param {(v: T) => PotentialPromise<U>} fn 
+   * @returns {Result<U>}
+   */
+  public map<U extends object>(fn: (v: T) => PotentialPromise<U>): Result<U> {
     const local = async () => {
       if (!this.isSuccess) return;
       const transform = await fn(this.value);
@@ -52,12 +83,22 @@ export class Result<T> {
     return this as unknown as Result<U>;
   }
 
+  /**
+   * Call the function passing the error when there is an error.
+   * @param {(e: Error) => PotentialPromise<void>} fn 
+   * @returns {Result<T>}
+   */
   public onFailure(fn: (e: Error) => PotentialPromise<void>): Result<T> {
     const local = async () => !this.isSuccess && (await fn(this.error));
     this.queue.push(local);
     return this;
   }
 
+  /**
+   * Call the function passing the encapsulated value when there is no error.
+   * @param {(v: T) => PotentialPromise<void>} fn 
+   * @returns {Result<T>}
+   */
   public onSuccess(fn: (v: T) => PotentialPromise<void>): Result<T> {
     const local = async () => this.isSuccess && (await fn(this.value));
     this.queue.push(local);
